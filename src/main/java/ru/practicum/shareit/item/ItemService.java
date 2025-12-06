50c2e5eb2ae129bb7d220231741ca16967e8de20
verp205 <s4naumenko@yandex.ru>
2025-12-07 01:22:12 +0300

    fix: CheckStyle

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,10 +5,15 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto createItem(Long ownerId, ItemDto dto);
+
     ItemDto updateItem(Long ownerId, Long itemId, ItemDto dto);
+
     ItemDto getItem(Long itemId);
+
     List<ItemDto> getItemsOfOwner(Long ownerId);
+
     List<ItemDto> search(String text);
 }
 
